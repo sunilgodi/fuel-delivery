f4d48ef7587863020b8a16d3589f443b883cdd49
Sunil kumar <Sunilkumargodi573@gmail.com>
2021-03-23 16:37:14 +0530

    initial commit

--- src/js/viewModels/about.js
+++ src/js/viewModels/about.js
@@ -4,7 +4,7 @@
  * Licensed under The Universal Permissive License (UPL), Version 1.0
  * as shown at https://oss.oracle.com/licenses/upl/
  * @ignore
- */
+ */ 
 /*
  * Your about ViewModel code goes here
  */
